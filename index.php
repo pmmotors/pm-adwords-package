3596b48d9a1700dc50f21ca6bdb81d6899f20286
Héctor Tello <hectortllo@gmail.com>
2021-07-02 18:41:49 -0600

    google-ads added to the project

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <?php
 
 require __DIR__ . '/vendor/autoload.php';
+
+$ad_words_id = "104-280-2798";
+//Getting session for each customerId        
+AdWords\AdWordsConnection::getSession($ad_words_id);
---
+++ src/AdWords/AdWordsConnection.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace AdWords;
+
+use Google\AdsApi\AdWords\AdWordsServices;
+use Google\AdsApi\AdWords\AdWordsSessionBuilder;
+use Google\AdsApi\Common\OAuth2TokenBuilder;
+use Google\AdsApi\Common\ConfigurationLoader;
+
+use Google\AdsApi\AdWords\v201809\mcm\CustomerService;
+use Google\AdsApi\AdWords\v201809\cm\Selector;
+use Google\AdsApi\AdWords\v201809\cm\ApiException;
+use Google\AdsApi\AdWords\v201809\cm\AuthenticationErrorReason;
+
+class AdWordsConnection
+{
+
+    public static function getSession($customerId)
+    {
+        $configuration = (new ConfigurationLoader())->fromFile('google-ads.ini');
+
+        // Generate a refreshable OAuth2 credential for authentication.
+        $oAuth2Credential = (new OAuth2TokenBuilder())
+            ->from($configuration)
+            ->build();
+
+        // Construct an API session configured from a properties file and the
+        // OAuth2 credentials above.
+        $session = (new AdWordsSessionBuilder())->from($configuration)->withOAuth2Credential($oAuth2Credential)
+            ->withClientCustomerID($customerId)
+            ->build();
+        return $session;
+    }
+
+    public function __construct()
+    {
+    }
+
+    public function validateAccountId($accountId)
+    {
+        $configuration = (new ConfigurationLoader())->fromFile(config('google-ads.ini'));
+
+        // Generate a refreshable OAuth2 credential for authentication.
+        $oAuth2Credential = (new OAuth2TokenBuilder())
+            ->from($configuration)
+            ->build();
+
+        $session = (new AdWordsSessionBuilder())
+            ->from($configuration) //->fromFile(env('GOOGLE_ADWORDS_INI'))
+            ->withOAuth2Credential($oAuth2Credential)
+            ->withClientCustomerID($accountId)
+            ->build();
+        $selector = new Selector();
+        $selector->setFields(['Id', 'Name']);
+        try {
+            //TEST SERVICE CALL
+            $service = (new AdWordsServices())->get($session, CustomerService::class);
+            $service->getCustomers();
+        } catch (ApiException $apiException) {
+            if (!$apiException->getErrors()[0]->getReason() == AuthenticationErrorReason::CUSTOMER_NOT_FOUND) {
+                throw $apiException; // validation can't be evaluated.
+            }
+            return false;
+        }
+        return true;
+    }
+}
