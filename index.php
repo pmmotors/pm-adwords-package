92419e8118099a60c338b7607491c3c35ee003c2
Héctor Tello <hectortllo@gmail.com>
2021-07-09 18:13:16 -0600

    Package working correctly with ...ReportBudget, ...CallExtension, ...ReportMaserati

--- index.php
+++ index.php
@@ -4,14 +4,15 @@ use PmAnalyticsPackage\AdWords\AdWordsConnection;
 use PmAnalyticsPackage\api\AdWords\AdWordsReportMaserati;
 use Carbon\Carbon;
 use PmAnalyticsPackage\api\AdWords\AdWordsReportBudget;
+use PmAnalyticsPackage\api\AdWords\AdWordsReportCallExtension;
 
 require __DIR__ . '/vendor/autoload.php';
 
 $ad_words_id = "104-280-2798";
 
 //Getting session for each customerId 
-// $apiAdWords = new AdWordsConnection();
-// $apiAdWords->getSession($ad_words_id);
+$apiAdWords = new AdWordsConnection();
+$apiAdWords->getSession($ad_words_id);
 
 // $maserati = new AdWordsReportMaserati(
 //     '338-219-1963',
@@ -22,7 +23,7 @@ $ad_words_id = "104-280-2798";
 
 // $maserati->getReport();
 
-$budget = new AdWordsReportBudget(
+$budget = new AdWordsReportCallExtension(
     '338-219-1963',
     Carbon::createFromDate(2021, 1, 7),
     Carbon::createFromDate(2021, 8, 7),
